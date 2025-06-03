b62db9c22e8ee092c1a2111b4d817f0b3af2457a
Ossama-Dev07 <ossamadev22@gmail.com>
2025-06-03 22:29:58 +0100

    AGO backend logic

---
+++ Backend/app/Models/AGO.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AGO extends Model
+{
+    use HasFactory;
+}
